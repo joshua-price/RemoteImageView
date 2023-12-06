26e705d82ad854af956ccfe6c36ad3501ecdc325
Joshua Price <jjprice@localiis.com>
2023-12-06 17:37:51 -0500

    RemoteImageFetcher  - add func purge()

--- Sources/RemoteImageView/RemoteImageFetcher.swift
+++ Sources/RemoteImageView/RemoteImageFetcher.swift
@@ -57,4 +57,9 @@ public class RemoteImageFetcher: ObservableObject {
   public func getUrl() -> URL {
     return url
   }
+    
+    public func purge() {
+        imageData = Data()
+    }
+    
 }
